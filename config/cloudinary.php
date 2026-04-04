8f6ef5455660cb13f722aca0728c1e0c23793b1f
yusfate4 <abiodunyusuf4@gmail.com>
2026-04-04 09:01:50 +0100

    Add Cloudinary config file

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    // 1. Try the full URL first
     'cloud_url' => env('CLOUDINARY_URL'),
-
-    // 2. Explicitly map the parts so Laravel doesn't get confused
+    
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key'    => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
